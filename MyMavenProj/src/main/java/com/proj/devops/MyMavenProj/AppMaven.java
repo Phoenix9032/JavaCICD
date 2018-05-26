e81e035a10da71e842e89125e7e2ab2b35ddfe37
= <mjolnir.thors@gmail.com>
2018-05-26 16:41:32 +0530

    First Java ICD

---
+++ MyMavenProj/src/main/java/com/proj/devops/MyMavenProj/AppMaven.java
@@ -0,0 +1,13 @@
+package com.proj.devops.MyMavenProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppMaven 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ MyMavenProj/src/test/java/com/proj/devops/MyMavenProj/AppMavenTest.java
@@ -0,0 +1,20 @@
+package com.proj.devops.MyMavenProj;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppMavenTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
